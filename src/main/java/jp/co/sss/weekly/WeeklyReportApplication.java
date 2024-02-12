e94b159ce009b36b2793935c3af501c2fb75c199
m-sasaki <x.ri.mm.s@gmail.com>
2024-02-12 14:53:10 +0900

    FirstCommit

---
+++ src/main/java/jp/co/sss/weekly/WeeklyReportApplication.java
@@ -0,0 +1,13 @@
+package jp.co.sss.weekly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklyReportApplication.class, args);
+	}
+
+}
